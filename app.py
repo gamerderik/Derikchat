4f7e3eb297fcf83a27afaa45c130eec3830de161
Derik1408 <85242612+gamerderik@users.noreply.github.com>
2024-11-30 22:24:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def save_message_to_firebase(username, message):
     ref.push({
         "username": username,
         "message": message,
-        "timestamp": db.ServerValue.TIMESTAMP
+        "timestamp": db.server_timestamp() 
     })
 
 
